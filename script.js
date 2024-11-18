a306b61bd2d99120d1e3c336e232580a8903d3a4
Simen Lien <lisis112024@gmail.com>
2024-11-18 13:39:35 +0100

    oppgave 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "Simen",
+    lastName: "Lien",
+    age: 31,
+    city: "Bergen"
+}
+
+console.log(person.firstName) // Simen
+console.log(person.lastName) // Lien
+console.log(person.length) // undefined
+console.log(Object.keys(person).length); // 4
